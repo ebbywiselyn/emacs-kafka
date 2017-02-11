76f3e002aa8d0948b51b0df36acf88f970912987
Ebby Wiselyn <ebby@scypho.com>
2017-02-11 19:17:12 +0530

    Refactor and add more code for deletion

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Attempts to create a kafka-mode in emacs.
 Starting a local kafka-broker, zookeeper, and console consumer/producer in emacs comint modes. 
 
 Using magit-popup to play around kafka-topics, consumer-groups CLI commands. 
+
+
+
+## Requirements
+	* magit libraries (install magit).
+	* Download kafka and set kafka-cli-path
--- popup.el
+++ popup.el
@@ -4,27 +4,28 @@
 
 (require 'magit-popup)
 
-(defvar kafka-cli-path "~/apps/kafka/kafka/bin/")
+(defcustom kafka-cli-path "~/apps/kafka/kafka/bin/" "Kafka CLI tools path.")
 
 (defun alter-topics (topic)
   "TOPIC ."
-  (interactive (list (completing-read "Topic:" (get-topics))))
+  (interactive (list (completing-read "Topic:" (--get-topics))))
   (message "alter topics %s" topic))
 
 (defun create-topics (topic)
   "TOPIC ."
-  (interactive (list (completing-read "Topic:" (get-topics))))
+  (interactive (list (completing-read "Topic:" (--get-topics))))
   (message "create topics %s" topic))
 
 (defun delete-topics (topic)
   "TOPIC ."
-  (interactive (list (completing-read "Topic:" (get-topics))))
-  (message "delete topics %s" topic))
+  (interactive (list (completing-read "Topic:" (--get-topics))))
+  (let* ((topics-cli (concat kafka-cli-path "/kafka-topics.sh"))
+	 (buff (get-buffer-create "*kafka-output*")))
+    (call-process topics-cli nil buff t "--zookeeper" "localhost:2181" "--topic" topic "--delete")))
 
 (defun describe-topics (topic)
   "TOPIC."
-  (interactive
-   (list (completing-read "Topic:" (get-topics))))
+  (interactive (list (completing-read "Topic:" (--get-topics))))
   (message "describe topics: %s" topic))
 
 (defun list-topics ()
@@ -36,11 +37,15 @@
       (message "topics-cli %s" topics-cli)
       (call-process topics-cli nil buff t "--zookeeper" "localhost:2181" "--list"))))
 
-(defun get-topics () ;; Name it like a private function
-  "."
-  (save-excursion
-    (with-current-buffer (get-buffer "*kafka-topics*")
-      (split-string (buffer-string)))))
+(defun --get-topics (&optional update) ;; Name it like a private function
+  "UPDATE."
+  (if (or  (not (boundp 'all-topics)) update)
+    (save-excursion
+      (list-topics)
+      (with-current-buffer (get-buffer "*kafka-topics*")
+	(setq all-topics (split-string (buffer-string)))))
+    all-topics))
+
 
 (magit-define-popup magit-kafka-topics
   "Some doc"
