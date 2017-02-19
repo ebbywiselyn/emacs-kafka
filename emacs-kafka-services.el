0548625618fa854f899df7af6727c11b4885a267
Ebby Wiselyn <ebby@scypho.com>
2017-02-19 19:12:42 +0530

    Rename files and also update README.md info

---
+++ emacs-kafka-services.el
@@ -0,0 +1,79 @@
+;;; emacs-kafka-services.el --- Summary
+;;; Commentary:
+;;; Code:
+
+(require 'popup)
+
+;;;###autoload
+(defvar zookeeper-cli-file-path (concat kafka-cli-bin-path "/zookeeper-server-start.sh")
+  "Path to the program used by `run-zookeeper'.")
+
+;;;###autoload
+(defvar zookeeper-cli-arguments (concat kafka-cli-config-path "/zookeeper.properties")
+  "Command line arguments to `zookeeper-server-start.sh'.")
+
+;;;###autoload
+(defun run-zookeeper (switch)
+  "Run Zookeeper switch to buffer if SWITCH is non 'nil."
+  (interactive "i")
+  (let* ((zookeeper-buffer-name "*zookeeper*")
+	 (zookeeper-buffer (get-buffer-create zookeeper-buffer-name)))
+    (if (comint-check-proc zookeeper-buffer)
+	(and switch (switch-to-buffer zookeeper-buffer-name))
+      (progn
+	(apply 'make-comint-in-buffer "*zookeeper*"
+	       zookeeper-buffer zookeeper-cli-file-path
+	       'nil (list zookeeper-cli-arguments))
+	(and switch (switch-to-buffer zookeeper-buffer)))
+      )))
+
+;;;###autoload
+(defvar kafka-broker-cli-file-path (concat kafka-cli-bin-path "/kafka-server-start.sh")
+  "Path to the program used by `run-kafka'.")
+
+;;;###autoload
+(defvar kafka-broker-cli-arguments (concat kafka-cli-config-path "/server.properties")
+  "Command line arguments to `kafka-server-start.sh'.")
+
+;;;###autoload
+(defun run-kafkabroker(switch)
+  "Run Kafka Broker"
+  (interactive "i")
+  (let* ((kafka-broker-buffer-name "*kafka*")
+	 (kafka-broker-buffer (get-buffer-create kafka-broker-buffer-name)))
+    (if (comint-check-proc kafka-broker-buffer)
+	(and switch (switch-to-buffer kafka-broker-buffer))
+      (progn
+	(apply
+	 'make-comint-in-buffer "*kafka*" kafka-broker-buffer
+	 kafka-broker-cli-file-path 'nil (list kafka-broker-cli-arguments))
+	(and switch (switch-to-buffer kafka-broker-buffer))))))
+
+;;;###autoload
+(defvar kafka-consumer-cli-file-path (concat kafka-cli-bin-path "/kafka-console-consumer.sh")
+  "Path to the program used by `run-kafka'.")
+
+;;;###autoload
+(defvar kafka-consumer-cli-arguments
+  '("--whitelist" "event.json.*" "--bootstrap-server" "localhost:9092")
+  "Command line arguments to `kafka-console-consumer.sh'.")
+
+;;;###autoload
+(defun run-kafkaconsumer(switch)
+  "Run Kafka Consumer"
+  (interactive "i")
+  (let* ((kafka-consumer-buffer-name "*consumer*")
+
+	 (kafka-consumer-buffer (get-buffer-create kafka-consumer-buffer-name)))
+    (if (comint-check-proc kafka-consumer-buffer)
+	(and switch (switch-to-buffer kafka-consumer-buffer))
+      (progn
+	(apply
+	 'make-comint-in-buffer "*consumer*" kafka-consumer-buffer
+	 kafka-consumer-cli-file-path 'nil kafka-consumer-cli-arguments)
+	(and switch (switch-to-buffer kafka-consumer-buffer))))))
+
+
+(provide 'emacs-kafka-services)
+
+;;; emacs-kafka-services.el ends here
---
+++ emacs-kafka.el
@@ -0,0 +1,108 @@
+;;; emacs-kafka.el --- Summary
+;;; Commentary:
+;;; Code:
+
+;; todo check if library exists else issue warning
+(require 'magit-popup)
+
+;;;###autoload
+(defcustom kafka-cli-bin-path "/home/ebby/apps/kafka/kafka/bin/" "Kafka CLI tools path.")
+
+;;;###autoload
+(defcustom kafka-cli-config-path "/home/ebby/apps/kafka/kafka/config/" "Kafka CLI config path.")
+
+;;;###autoload
+(defun alter-topics (topic)
+  "Edit the TOPIC ."
+  (interactive (list (completing-read "Topic:" (--get-topics))))
+  (message "alter topics %s" topic))
+
+;;;###autoload
+(defun create-topics (topic partition)
+  "Create the TOPIC ."
+  (interactive "sTopic: \nsPartition:")
+  (let* ((topics-cli (concat kafka-cli-bin-path "/kafka-topics.sh"))
+	 (buff (get-buffer-create "*kafka-output*")))
+    (call-process topics-cli nil buff t "--zookeeper" "localhost:2181" "--topic" topic "--partition" partition "--replication-factor" "1" "--create")))
+
+;;;###autoload
+(defun delete-topics (topic)
+  "Delete the TOPIC ."
+  (interactive (list (completing-read "Topic:" (--get-topics))))
+  (let* ((topics-cli (concat kafka-cli-bin-path "/kafka-topics.sh"))
+	 (buff (get-buffer-create "*kafka-output*")))
+    (call-process topics-cli nil buff t "--zookeeper" "localhost:2181" "--topic" topic "--delete")))
+
+;;;###autoload
+(defun describe-topics (topic)
+  "Describe the topic partition, replication factor, configs of TOPIC."
+  (interactive (list (completing-read "Topic:" (--get-topics))))
+  (let* ((topics-cli (concat kafka-cli-bin-path "/kafka-topics.sh"))
+	 (buff (get-buffer-create "*kafka-output*")))
+    (call-process topics-cli nil buff t
+		  "--zookeeper" "localhost:2181" "--topic" topic "--describe")
+    (switch-to-buffer-other-window "*kafka-output*")
+    (kafka-topic-mode)))
+
+;;;###autoload
+(defun list-topics ()
+  "List all the topics in the zookeeper ."
+  (interactive) ;; Refer magit how to write your own list buffer mode?
+  (let* ((buff "*kafka-topics*")
+	 (topics-cli (concat kafka-cli-bin-path "/kafka-topics.sh")))
+    (call-process topics-cli nil buff t "--zookeeper" "localhost:2181" "--list")
+    (switch-to-buffer-other-window "*kafka-topics*")
+    (kafka-topic-mode)))
+
+;;;###autoload
+(defun --get-topics (&optional update) ;; Name it like a private function
+  "UPDATE."
+  (if (or  (not (boundp 'all-topics)) update)
+    (save-excursion
+      (list-topics)
+      (with-current-buffer (get-buffer "*kafka-topics*")
+	(setq all-topics (split-string (buffer-string)))))
+    all-topics))
+
+;;;###autoload
+(magit-define-popup magit-kafka-topics
+  "Some doc"
+  :actions '((?a "Alter Topics" alter-topics)
+	     (?c "Create Topics" create-topics)
+	     (?d "Delete Topics" delete-topics)
+	     (?h "Describe Topics" describe-topics)
+	     (?l "List all Topicss" list-topics))
+  :default-action 'describe-topics)
+
+(defvar kafka-topic-mode-map
+  (let ((map (make-keymap)))
+    (define-key map (kbd "q") 'bury-buffer)
+    map)
+  "Keymap for `kafka-topic-mode'.")
+
+(defvar kafka-topic-highlights
+       '((
+	  ("Topic\\|PartitionCount\\|Configs\\|Leader\\|Replicas\\|Isr\\|ReplicationFactor\\|Partition\\|Group\\|Broker" . font-lock-keyword-face)
+	  (":\\|,\\|;\\|{\\|}\\|=>\\|@\\|$\\|=" . font-lock-string-face)
+	  )))
+
+(define-derived-mode kafka-topic-mode special-mode "Kafka Topics"
+  "Mode for looking at kafka topics.
+\\{kafka-topic-mode-map}"
+  :group 'kafka-topics
+  (use-local-map kafka-topic-mode-map)
+  (setq font-lock-defaults kafka-topic-highlights)
+  (setq buffer-read-only 'nil))
+
+;; (require 'logview)
+;; (define-derived-mode kafka-process-mode comint-mode "Kafka Processes"
+;;   "Mode for looking at kafka processes
+;;   \\\\{kafka-process-mode-map}"
+;;   :group 'kafka-process
+;;   (use-local-map logview-mode-map))
+
+
+
+(provide 'emacs-kafka)
+
+;;; emacs-kafka.el ends here
