215c657214df1594218251d6c52a30fe48c926d0
Ebby Jeyapaul <ejeyapaul@demonware.net>
2017-10-20 15:58:54 -0700

    Fix bug where custom variables are not set, don't use defvar

--- kafka-cli-custom.el
+++ kafka-cli-custom.el
@@ -26,13 +26,5 @@
   :group 'kafka-cli
   )
 
-;;;###autoload
-(defvar zookeeper-cli-file-path (concat kafka-cli-bin-path "/zookeeper-server-start.sh")
-  "Path to the program used by `run-zookeeper'.")
-
-;;;###autoload
-(defvar zookeeper-cli-arguments (concat kafka-cli-config-path "/zookeeper.properties")
-  "Command line arguments to `zookeeper-server-start.sh'.")
-
 (provide 'kafka-cli-custom)
 ;;; kafka-cli-custom.el ends here
--- kafka-cli-services.el
+++ kafka-cli-services.el
@@ -3,66 +3,88 @@
 ;;; Code:
 
 ;;;###autoload
+(defun get-zookeeper-cli-file-path ()
+  "."
+  (concat kafka-cli-bin-path "/zookeeper-server-start.sh"))
+
+;;;###autoload
+(defun get-zookeeper-cli-arguments ()
+  "."
+  (concat kafka-cli-config-path "/zookeeper.properties"))
+
+;;;###autoload
+(defun get-kafka-broker-cli-file-path ()
+  "Path to the program used by `run-kafka'."
+  (concat kafka-cli-bin-path "/kafka-server-start.sh"))
+
+;;;###autoload
+(defun get-kafka-broker-cli-arguments ()
+  "Command line arguments to `kafka-server-start.sh'."
+  (concat kafka-cli-config-path "/server.properties"))
+
+
+;;;###autoload
+(defun get-kafka-consumer-cli-file-path ()
+  "Path to the program used by `run-kafka'."
+  (concat kafka-cli-bin-path "/kafka-console-consumer.sh"))
+
+;;;###autoload
+(defun get-kafka-consumer-cli-arguments ()
+  "Command line arguments to `kafka-console-consumer.sh'."
+  `("--whitelist" "event.json.*" "--consumer-property" "group.id=kafka-cli-consumer" "--bootstrap-server" ,kafka-url))
+
+
+;;;###autoload
 (defun run-zookeeper (switch)
   "Run Zookeeper switch to buffer if SWITCH is non 'nil."
   (interactive "i")
   (let* ((zookeeper-buffer-name "*zookeeper*")
-	 (zookeeper-buffer (get-buffer-create zookeeper-buffer-name)))
+	 (zookeeper-buffer (get-buffer-create zookeeper-buffer-name))
+	 (zookeeper-cli-args (get-zookeeper-cli-arguments))
+	 (zookeeper-cli-file-path (get-zookeeper-cli-file-path))
+	 )
     (if (comint-check-proc zookeeper-buffer)
 	(and switch (switch-to-buffer zookeeper-buffer-name))
       (progn
 	(apply 'make-comint-in-buffer "*zookeeper*"
 	       zookeeper-buffer zookeeper-cli-file-path
-	       'nil (list zookeeper-cli-arguments))
+	       'nil (list zookeeper-cli-args))
 	(and switch (switch-to-buffer zookeeper-buffer))))
       (with-current-buffer zookeeper-buffer
 	(kafka-cli-log-mode))))
-
-;;;###autoload
-(defvar kafka-broker-cli-file-path (concat kafka-cli-bin-path "/kafka-server-start.sh")
-  "Path to the program used by `run-kafka'.")
-
-;;;###autoload
-(defvar kafka-broker-cli-arguments (concat kafka-cli-config-path "/server.properties")
-  "Command line arguments to `kafka-server-start.sh'.")
 
 ;;;###autoload
 (defun run-kafkabroker(switch)
   "Run Kafka Broker, switch to buffer if SWITCH is non 'nil."
   (interactive "i")
   (let* ((kafka-broker-buffer-name "*kafka*")
-	 (kafka-broker-buffer (get-buffer-create kafka-broker-buffer-name)))
+	 (kafka-broker-buffer (get-buffer-create kafka-broker-buffer-name))
+	 (kafka-broker-cli-args (get-kafka-broker-cli-arguments))
+	 (kafka-broker-cli-file-path (get-kafka-broker-cli-file-path)))
     (if (comint-check-proc kafka-broker-buffer)
 	(and switch (switch-to-buffer kafka-broker-buffer))
       (progn
 	(apply
 	 'make-comint-in-buffer "*kafka*" kafka-broker-buffer
-	 kafka-broker-cli-file-path 'nil (list kafka-broker-cli-arguments))
+	 kafka-broker-cli-file-path 'nil (list kafka-broker-cli-args))
 	(and switch (switch-to-buffer kafka-broker-buffer))))
       (with-current-buffer kafka-broker-buffer
 	(kafka-cli-log-mode))))
-
-;;;###autoload
-(defvar kafka-consumer-cli-file-path (concat kafka-cli-bin-path "/kafka-console-consumer.sh")
-  "Path to the program used by `run-kafka'.")
-
-;;;###autoload
-(defvar kafka-consumer-cli-arguments
-  `("--whitelist" "event.json.*" "--consumer-property" "group.id=kafka-cli-consumer" "--bootstrap-server" ,kafka-url)
-  "Command line arguments to `kafka-console-consumer.sh'.")
 
 ;;;###autoload
 (defun run-kafkaconsumer (switch)
   "Run Kafka Consumer, switch to buffer if SWITCH is non 'nil."
   (interactive "i")
   (let* ((kafka-consumer-buffer-name "*consumer*")
-	 (kafka-consumer-buffer (get-buffer-create kafka-consumer-buffer-name)))
+	 (kafka-consumer-buffer (get-buffer-create kafka-consumer-buffer-name))
+	 (kafka-consumer-cli-file-path (get-kafka-consumer-cli-file-path))
+	 (kafka-consumer-cli-args (get-kafka-consumer-cli-arguments)))
     (if (comint-check-proc kafka-consumer-buffer)
 	(and switch (switch-to-buffer kafka-consumer-buffer))
       (progn
 	(apply
 	 'make-comint-in-buffer "*consumer*" kafka-consumer-buffer
-	 kafka-consumer-cli-file-path 'nil kafka-consumer-cli-arguments)
+	 kafka-consumer-cli-file-path 'nil kafka-consumer-cli-args)
 	(and switch (switch-to-buffer kafka-consumer-buffer))))
     (with-current-buffer kafka-consumer-buffer
       (kafka-cli-log-mode))))
